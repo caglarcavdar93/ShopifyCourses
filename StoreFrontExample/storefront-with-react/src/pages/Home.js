1630492e4696103199046d16e035707d7fc80efa
Çağlar Çavdar <caglar.cavdar@windowslive.com>
2022-05-06 22:48:23 +0300

    show products images on home page

--- StoreFrontExample/storefront-with-react/src/pages/Home.js
+++ StoreFrontExample/storefront-with-react/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/shopContext";
+import { Box, Grid, Image, Text } from "@chakra-ui/react";
 function Home() {
   const { fetchAllProducts, products } = useContext(ShopContext);
   useEffect(() => {
@@ -10,15 +11,21 @@ function Home() {
   if (!products) return <div>Loading...</div>;
 
   return (
-    <div>
-      {products.map((product) => {
-        return (
-          <Link to={`/products/${product.handle}`} key={product.title}>
-            {product.title}
-          </Link>
-        );
-      })}
-    </div>
+    <Box>
+      <Grid templateColumns="repeat(3,1fr)">
+        {products.map((product) => {
+          return (
+            <Link to={`/products/${product.handle}`} key={product.title}>
+              <Box _hover={{ opacity: "80%" }} textAlign="center">
+                <Image src={product.images[0].src} />
+                <Text>{product.title}</Text>
+                <Text>{product.variants[0].price}</Text>
+              </Box>
+            </Link>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 }
 
