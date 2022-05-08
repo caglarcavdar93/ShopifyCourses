0ea160047ab9ec5bf1619464bcb58da315a821a2
Çağlar Çavdar <caglar.cavdar@windowslive.com>
2022-05-09 00:20:22 +0300

    Created footer and add herojs

--- StoreFrontExample/storefront-with-react/src/App.js
+++ StoreFrontExample/storefront-with-react/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import NavMenu from "./components/NavMenu";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
@@ -16,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products/:handle" element={<ProductPage />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
